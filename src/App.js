c6b6c6ded352bae274a6c54e2ce72c252f697e65
mutiat <mutiat900@gmail.com>
2024-02-03 10:00:07 +0100

    changed default city

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Brisbane" />
+        <Weather defaultCity="Lagos" />
         <Footer />
       </div>
     </div>
